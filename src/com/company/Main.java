b20440bc3afcb6fd040df92659cacfbd526472d7
simonasSimas <sasimas198@gmail.com>
2021-02-23 22:07:22 +0200

    First solo practice excericise done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) throws VaziuoklesIsskleidimoKlaida {
-//        Adresas adresas = new Adresas(new Miestas("Alytus"));
-//        System.out.println(gautiMiestoPavadinima(adresas));
-//    }
-//    static String gautiMiestoPavadinima(Adresas adresas){
-//        String miestoPavadinimas = "";
-//        try {
-//            miestoPavadinimas = adresas.getMiestas().getPavadinimas();
-//        }catch (Exception e){
-//            System.out.println("Null objektas");
-//        }
-//        return miestoPavadinimas;
-//    }
-//        Lektuvas lektuvas = new Lektuvas();
-//        lektuvas.isskelistiVaziuokle();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Iveskite intervalo pradzia: ");
+        int a = sc.nextInt();
+        System.out.println();
+        System.out.print("Iveskite intervalo pabaiga: ");
+        int b = sc.nextInt();
+        System.out.println();
+        boolean x = true;
+        for (int i = a; i <= b; i++) {
+            if (x) {
+                System.out.print("[ ");
+                x = false;
+                i--;
+            } else if ((b - 1 == i) && (i % 2 == 0)) {
+                System.out.print(i+" ]");
+            } else if ((i < b) && (i % 2 == 0)) {
+                System.out.print(i + ", ");
+
+            } else if ((i == b) && (i % 2 == 0)) {
+                System.out.print(i + " ]");
+            }
+        }
     }
 }
