06e3ee6db4365b542d59bf5ef7e7b22b6cc6b0cb
simonasSimas <sasimas198@gmail.com>
2021-02-23 20:17:28 +0200

    Null excercise 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        Adresas adresas = new Adresas(new Miestas("Alytus"));
+        System.out.println(gautiMiestoPavadinima(adresas));
+    }
+    static String gautiMiestoPavadinima(Adresas adresas){
+        String miestoPavadinimas = "";
+        try {
+            miestoPavadinimas = adresas.getMiestas().getPavadinimas();
+        }catch (Exception e){
+            System.out.println("Null objektas");
+        }
+        return miestoPavadinimas;
     }
 }
