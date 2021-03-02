58485b9ba25fba59f2d82dbd06d19fa66fd4985e
simonasSimas <sasimas198@gmail.com>
2021-03-02 14:53:48 +0200

    Polymorfism exc 2 done?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
 
 
-import com.company.polimorfizmas.L;
+import com.company.polimorfizmas.*;
 
 public class Main {
 
     public static void main(String[] args) {
+//        Draudimas draudimas = new TPVCADraudimas("John Doe", 1000, "ABC123", "Lietuva");
+//        System.out.println(draudimas.getApdraustojoVardas());
+//        System.out.println(draudimas.getDraudimoSuma());
+//        System.out.println(((AutomobilioDraudimas) draudimas).getAutomobilioValstNr());
+//        System.out.println(((TPVCADraudimas) draudimas).getGaliojimoTeritorija());
+//        System.out.println(((KaskoDraudimas)draudimas).getFransize());
+//        AutomobilioDraudimas automobilioDraudimas = new AutomobilioDraudimas("Simonas", 200, "AAA111");
+//        System.out.println(((KaskoDraudimas)automobilioDraudimas).getFransize());
+
     }
 }
